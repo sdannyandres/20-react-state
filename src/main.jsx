0c973e3331c97f75ca6a74649c60f75c1b89d642
sdannyandres <sdannyandres@gmail.com>
2022-11-17 16:57:20 +0100

    post

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,40 @@
-import {useEffect, useState} from 'react';
-import {createRoot} from 'react-dom/client'
+import { useEffect, useState } from 'react'
+import { createRoot } from 'react-dom/client'
 const root = document.getElementById('root')
-const rootReat =createRoot(root)
+const rootReat = createRoot(root)
 
-const Post = ({numero}) => {
+const Post = ({ numero }) => {
     const [data, setData] = useState(null)
-    const [c, setC] = useState(0)    
+    const [c, setC] = useState(0)
     console.log("render")
-    usseEffect (() => {
+    useEffect(() => {
         console.log("acceso a la base de datos")
-        fetch ('https://jsonplaceholder.typicode.com/posts/${numero}')
-        .then(response => response.json())
-        .then(data => setData(data))
-    }, [])
-    
+        fetch('https://jsonplaceholder.typicode.com/posts/${numero}')
+            .then(response => response.json())
+            .then(data => setData(data))
+    })
+
     if (!data) return <div>
-          cargando el post {numero}
-          <hr></hr>
+        cargando el post {numero}
+        <hr></hr>
     </div>
-   return <div>
-    <button onClick={() => setC(c + 1)}>Sumar {c}</button>
-    post {numero}
-    {JSON.stringify(data)}
-    <hr></hr>
-   </div>
-   
+
+    return <div>
+        <button onClick={() => setC(c + 1)}>Sumar {c}</button>
+        post {numero}
+        {JSON.stringify(data)}
+        <hr></hr>
+    </div>
 }
- 
+const App = () => {
+    return <div>
+        <Post numero={1}></Post> 
+        <Post numero={2}></Post> 
+        <Post numero={3}></Post>
+        <Post numero={4}></Post>
+                                    
+    </div>
+}
+
+rootReat.render(<App />)
 
