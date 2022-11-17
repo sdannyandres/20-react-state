7851bab8189ca30b88891ee6fe911a35cb91676a
sdannyandres <sdannyandres@gmail.com>
2022-11-17 19:57:30 +0100

    reset

--- src/main.jsx
+++ src/main.jsx
@@ -15,33 +15,8 @@ const useContador = () => {
     decrementar,
     reset
   }
-
-const Post = ({ numero }) => {
-    const [data, setData] = useState(null)
-    const [c, setC] = useState(0)
-    console.log("render")
-    useEffect(() => {
-        console.log("acceso a la base de datos", numero)
-        fetch('https://jsonplaceholder.typicode.com/posts/${numero}')
-            .then(response => response.json())
-            .then(data => setData(data))
-    })
-
-    if (!data) return <div>
-        cargando el post {numero}
-        <hr></hr>
-    </div>
-
-
-    return <div>
-        <button onClick={() => setC(c + 1)}>Sumar {c}</button>
-        post {numero}
-        {JSON.stringify(data)}
-        <hr></hr>
-    </div>
 }
 const App = () => {
-
   const { contador, incrementar, decrementar, reset } = useContador()
   return <div>
     {contador}
@@ -56,7 +31,7 @@ rootReat.render(
 
   </App>
 )
-}
+
 
 
 
